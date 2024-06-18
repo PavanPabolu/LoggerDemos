67e16c494c3be87630763b0a01923b45f96e7e14
PavanPabolu <PavanPabolu.edu@gmail.com>
2024-06-19 03:42:59 +0530

    settings

---
+++ Logger.AzureApplicationInsight.WebAPI/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Logger.AzureApplicationInsight.WebAPI
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        public readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        // GET: api/<HomeController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            
+            
+            _logger.LogInformation("This is test message from Logger.AzureApplication.WebAPI controller - {DT}.", DateTime.UtcNow.ToLongTimeString());
+
+
+
+
+
+
+
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<HomeController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<HomeController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<HomeController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<HomeController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- Logger.AzureApplicationInsight.WebAPI/Program.cs
+++ Logger.AzureApplicationInsight.WebAPI/Program.cs
@@ -1,4 +1,16 @@
+using Microsoft.Extensions.Logging;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+//var configuration = new ConfigurationBuilder()
+//    .AddJsonFile("appsettings.json")
+//    .Build();
+builder.Logging.ClearProviders();
+//builder.Logging.AddConsole();
+builder.Logging.AddApplicationInsights("0c4e73f1-8e95-4723-87a4-c863e6b2e742");
+
+
 
 // Add services to the container.
 
@@ -18,6 +30,7 @@ builder.Services.AddApplicationInsightsTelemetry();
 
 var app = builder.Build();
 
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -30,5 +43,23 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+
+
+
+//-----------------------------------------------
+
+app.Logger.LogTrace("Adding Routes");
+app.MapGet("/", () => "Hello World!");
+app.MapGet("/Test", async (ILogger<Program> logger, HttpResponse response) =>
+{
+    logger.LogInformation("Testing logging in Program.cs");
+    await response.WriteAsync("Testing");
+});
+app.Logger.LogInformation("Starting the app");
+
+//-----------------------------------------------
+
+
 
 app.Run();
