83c173da18d4547ea66faf637542fffa60799d61
PavanPabolu <PavanPabolu.edu@gmail.com>
2024-06-19 23:32:22 +0530

    multiple endpoints

--- Logger.HTTPlogging.WebAPI/Program.cs
+++ Logger.HTTPlogging.WebAPI/Program.cs
@@ -1,7 +1,6 @@
 using Logger.HTTPlogging.WebAPI.Common;
 using Microsoft.AspNetCore.HttpLogging;
 using Microsoft.Extensions.Logging;
-//using Microsoft.Extensions.Logging;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,7 +14,7 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddHttpLogging(logging =>
 {
     logging.LoggingFields = HttpLoggingFields.All; //Microsoft.AspNetCore.HttpLogging
-    //logging.ResponseHeaders.Add("MyResponseHeader");
+    logging.ResponseHeaders.Add("MyResponseHeader");
     //logging.MediaTypeOptions.AddText("application/javascript");
     //logging.RequestBodyLogLimit = 4096; // Log up to 4KB of the request body
     //logging.ResponseBodyLogLimit = 4096;
@@ -29,7 +28,6 @@ builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
 builder.Logging.AddConsole();
 //builder.Logging.AddFile(logFilePath); // Requires Serilog.Extensions.Logging.File package
 builder.Logging.AddProvider(new FileLoggerProvider(logFilePath));
-
 
 //---------------------------------------------------
 
@@ -45,26 +43,22 @@ if (app.Environment.IsDevelopment())
 }
 
 //---------------------------------------------------
-// Use HTTP Logging Middleware: Adds the middleware to the request pipeline to log the HTTP requests and responses.
+//Use HTTP Logging Middleware: Adds the middleware to the request pipeline to log the HTTP requests and responses. Keep app.UseHttpLogging() after the swagger pipeline.
 app.UseHttpLogging();
 //---------------------------------------------------
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 //---------------------------------------------------
-
+/*
 // Set up logging
 var logger = LoggerFactory.Create(builder =>
 {
-    builder
-        .AddConsole()
-        .AddProvider(new FileLoggerProvider($"{Path.GetTempPath()}/Logs/http-requests.log"));
+    builder.AddConsole()
+           .AddProvider(new FileLoggerProvider($"{Path.GetTempPath()}/Logs/http-requests.log"));
 }).CreateLogger("HTTPLogger");
-
 
 // Middleware to log requests and responses
 app.Use(async (context, next) =>
@@ -73,7 +67,7 @@ app.Use(async (context, next) =>
     await next.Invoke();
     logger.LogInformation($"HTTP Response: {context.Response.StatusCode}");
 });
-
+*/
 app.Use(async (context, next) =>
 {
     context.Response.Headers["MyResponseHeader"] = new string[] { "My Response Header Value" };
@@ -81,6 +75,15 @@ app.Use(async (context, next) =>
 });
 
 app.MapGet("/", () => "\n\nHello World! This is Http-Logging.");
+
+//For endpoint-specific configuration in minimal API apps
+app.MapGet("/response", () => "Hello World! (logging response)")
+    .WithHttpLogging(HttpLoggingFields.ResponsePropertiesAndHeaders);
+
+//For endpoint-specific configuration in apps that use controllers,
+app.MapGet("/duration", [HttpLogging(loggingFields: HttpLoggingFields.Duration)]
+() => "Hello World! (logging duration)");
+
 //---------------------------------------------------
 
 app.Run();
