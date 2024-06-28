51ff70209bc7c038a2de013a60ae589c29a983f2
PavanPabolu <PavanPabolu.edu@gmail.com>
2024-06-29 00:48:48 +0530

    using Start/StopWatch library

--- Logger.AzureApplicationInsight.UsageMonitor.WebApp/Common/Middlewares/ApplicationInsightsMiddleware.cs
+++ Logger.AzureApplicationInsight.UsageMonitor.WebApp/Common/Middlewares/ApplicationInsightsMiddleware.cs
@@ -1,5 +1,6 @@
 ﻿
 using Microsoft.ApplicationInsights;
+using System.Diagnostics;
 
 namespace Logger.AzureApplicationInsight.UsageMonitor.WebApp.Common.Middlewares
 {
@@ -12,15 +13,34 @@ namespace Logger.AzureApplicationInsight.UsageMonitor.WebApp.Common.Middlewares
             _telemetryClient = telemetryClient;
         }
 
+        //public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        //{
+        //    //Track PAge view
+        //    _telemetryClient.TrackPageView($"Page view: {context.Request.Path}");
+
+        //    //Call the next middleware in the pipeline
+        //    await next(context);
+
+        //    //Track Page Load time
+        //    var pageLoadTime = context.Items["PageLoadTime"] as double?;
+        //    if (pageLoadTime.HasValue)
+        //        _telemetryClient.TrackMetric("Page Load Time", pageLoadTime.Value);
+
+        //}
+
         public async Task InvokeAsync(HttpContext context, RequestDelegate next)
         {
             //Track PAge view
             _telemetryClient.TrackPageView($"Page view: {context.Request.Path}");
 
             //Call the next middleware in the pipeline
+            var stopwatch = Stopwatch.StartNew();
             await next(context);
+            stopwatch.Stop();
 
             //Track Page Load time
+            context.Items["PageLoadTime"] = stopwatch.ElapsedMilliseconds;
+
             var pageLoadTime = context.Items["PageLoadTime"] as double?;
             if (pageLoadTime.HasValue)
                 _telemetryClient.TrackMetric("Page Load Time", pageLoadTime.Value);
